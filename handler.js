b9ca1fd263d0fe380067419877ded606d7dc429f
Patrick Lake <plake492@gmail.com>
2021-09-07 09:47:27 -0400

    Init

---
+++ handler.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `THE ENV VARIABLE IS ${process.env.MESSAGE}`,
+        input: event
+      },
+      null,
+      2
+    )
+  }
+}
